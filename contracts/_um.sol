1af40353fbd9e07137b74a826a2f7f77514381f9
PhiMarHal <77635908+PhiMarHal@users.noreply.github.com>
2022-06-18 20:56:17 +0200

    decreased mint curve to +10bps
    Gives us a "realistic" supply of 5000-6000, in the unlikely chance this takes off!

--- contracts/_um.sol
+++ contracts/_um.sol
@@ -1,5 +1,12 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: LYAA
 
+// _undefined mutables, a NFT collection
+//
+// token metadata is taken from existing collections
+// this metadata changes with every block
+// in theory supply is infinite
+// but mint cost raises with each mint
+// by #5555, each mint would cost more than 1 ETH
 
 pragma solidity ^0.8.14;
 
@@ -10,8 +17,8 @@ contract _um is ERC721, Ownable {
 
     string private _contractURI                     = "";
     uint256 public totalSupply                      = 0;
-    uint256 public mintCost                         = 0.004 ether;
-    uint256 constant public RAISE_MINT_COST_BPS     = 200;
+    uint256 public mintCost                         = 0;
+    uint256 constant public RAISE_MINT_COST_BPS     = 10; // 0.1%
 
     struct Collection {
         string name;
@@ -21,8 +28,9 @@ contract _um is ERC721, Ownable {
 
     Collection[] public collect ;
 
-    constructor() ERC721("_undefined mutables", "_UM") {
+    constructor(uint256 _mintCost) ERC721("_undefined mutables", "_UM") {
         transferOwnership(msg.sender);
+        mintCost = _mintCost; // 4000000000000000 for 0.004 eth
     }
 
     // _um collection metadata for marketplaces
@@ -36,12 +44,11 @@ contract _um is ERC721, Ownable {
     }
 
     // pick a pseudorandom URI for a given token
-    // then return a random tokenId for that collection
+    // then return a pseudorandom tokenId for that collection
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         _requireMinted(tokenId);
 
-        // add 2 to avoid multiplication by 0 or 1 with those token ids
-        uint256 _pseudorando = block.timestamp * (tokenId + 2);
+        uint256 _pseudorando = block.timestamp + tokenId;
         
         // get a random collection
         string memory _currentURI = collect[_pseudorando % collect.length].baseURI;
@@ -56,11 +63,14 @@ contract _um is ERC721, Ownable {
     // in the future, ownership could be transferred to a contract
     // to open up permissionless collection management
 
-    // add any collection, specifying baseURI and totalSupply
-    function addCollection(string memory _name, string memory _uri, uint256 _supply) onlyOwner public {
+    // add any number of collections, specifying name, baseURI and totalSupply
+    function addCollection(string[] memory _name, string[] memory _uri, uint256[] memory _supply) onlyOwner public {
+        require(_name.length == _uri.length && _name.length == _supply.length, "_um: mismatched array lengths");
 
-        Collection memory addedCollection = Collection(_name, _uri, _supply);
-        collect.push(addedCollection);
+        for(uint256 i = 0; i < _name.length; i++) {
+            Collection memory addedCollection = Collection(_name[i], _uri[i], _supply[i]);
+            collect.push(addedCollection);
+        }
     }
 
     // change baseURI or totalSupply for an existing collection
@@ -77,10 +87,9 @@ contract _um is ERC721, Ownable {
         collect.pop();
     }
     
-    // minting is technically infinite but pragmatically capped by proportional cost growth 
-
+    // mint a new mutable and raise the mint cost
     function mint(address _recipient) public payable {
-        require(msg.value >= mintCost, "UM: not enough eth to mint");
+        require(msg.value >= mintCost, "_um: not enough eth to mint");
 
         payable(owner()).transfer(mintCost);
         mintCost += mintCost * RAISE_MINT_COST_BPS / 10000;
